27e98becbba9297954c1f25e7a18edd365309ed2
artur-mrozek <artur.mrozek1@gmail.com>
2023-11-05 13:54:32 +0100

    registered models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pizza, Order, OrderItem
+
 
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Order)
+admin.site.register(OrderItem)
