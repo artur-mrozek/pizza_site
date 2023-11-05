238a84879e6830b0968696957512479be3c7494b
artur-mrozek <artur.mrozek1@gmail.com>
2023-11-05 13:02:12 +0100

    created home page

--- orders/models.py
+++ orders/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Pizza(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     price_medium_size = models.FloatField()
     price_large_size = models.FloatField()
     image = models.ImageField()
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    address = models.CharField()
+    address = models.CharField(max_length=20)
     is_done = models.BooleanField()
 
 class OrderItem(models.Model):
