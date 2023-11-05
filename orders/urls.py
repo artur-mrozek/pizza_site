b18159a4be22527aec9eb9f9dd5fe69604aa4161
artur-mrozek <artur.mrozek1@gmail.com>
2023-11-05 13:11:06 +0100

    login, logout, register

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, login_view, logout_view, register_view
 
 app_name = "orders"
 urlpatterns = [
-    path('', home_view, name="home")
+    path('', home_view, name="home"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+    path('register/', register_view, name="register"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "orders/home.html", {})
+
+def login_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        return redirect("/")
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/login/')
+    context = {"form": form}
+    return render(request, "orders/login.html", context)
+
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return redirect("/")
+
+def register_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        return redirect("/")
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+            user.save()
+            login(request, user)
+            return redirect('/')
+    context = {"form": form}
+    return render(request, "orders/register.html", context)
