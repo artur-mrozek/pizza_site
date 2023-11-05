cfdb39c4b318e808401c1d016877d82d2a34bad5
artur-mrozek <artur.mrozek1@gmail.com>
2023-11-05 12:40:51 +0100

    created models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField()
+    price_medium_size = models.FloatField()
+    price_large_size = models.FloatField()
+    image = models.ImageField()
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    address = models.CharField()
+    is_done = models.BooleanField()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, null=False)
+    size = models.CharField(max_length=1)
